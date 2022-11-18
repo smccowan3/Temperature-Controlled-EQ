91e2cbe453d407132fb4e85ce84a7eea5da21a82
smccowan3 <68258345+smccowan3@users.noreply.github.com>
2022-11-18 13:57:20 +0900

    architecture for EQ parameters
    low cut peak freq and high cut added. Can add more later

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -98,7 +98,30 @@ void TemperatureSliderAudioProcessor::changeProgramName (int index, const juce::
 //==============================================================================
 void TemperatureSliderAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
+    juce::dsp::ProcessSpec spec;
+        
+        spec.maximumBlockSize = samplesPerBlock;
+        
+        spec.numChannels = 1;
+        
+        spec.sampleRate = sampleRate;
+        
+        leftChain.prepare(spec);
+        rightChain.prepare(spec);
+        
+        updateFilters();
+        
+        leftChannelFifo.prepare(samplesPerBlock);
+        rightChannelFifo.prepare(samplesPerBlock);
+        
+        osc.initialise([](float x) { return std::sin(x); });
+        
+        spec.numChannels = getTotalNumOutputChannels();
+        osc.prepare(spec);
+        osc.setFrequency(440);
 
+    
+    
     // Use this method as the place to do any pre-playback
     // initialisation that you need..
 }
@@ -150,7 +173,12 @@ void TemperatureSliderAudioProcessor::processBlock (juce::AudioBuffer<float>& bu
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
+    
+    updateFilters();
+   
+   juce::dsp::AudioBlock<float> block(buffer);
+       
+    // This is the place where you'd normally do theguts of your plugin's
     // audio processing...
     // Make sure to reset the state if your inner loop is processing
     // the samples and the outer loop is handling the channels.
@@ -162,6 +190,19 @@ void TemperatureSliderAudioProcessor::processBlock (juce::AudioBuffer<float>& bu
 
         // ..do something to the data...
     }
+    
+   auto leftBlock = block.getSingleChannelBlock(0);
+   auto rightBlock = block.getSingleChannelBlock(1);
+   
+   juce::dsp::ProcessContextReplacing<float> leftContext(leftBlock);
+   juce::dsp::ProcessContextReplacing<float> rightContext(rightBlock);
+   
+   leftChain.process(leftContext);
+   rightChain.process(rightContext);
+   
+   leftChannelFifo.update(buffer);
+   rightChannelFifo.update(buffer);
+    
 }
 
 //==============================================================================
@@ -181,12 +222,22 @@ void TemperatureSliderAudioProcessor::getStateInformation (juce::MemoryBlock& de
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
+    
+    juce::MemoryOutputStream mos(destData, true);
+    apvts.state.writeToStream(mos);
 }
 
 void TemperatureSliderAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+       if( tree.isValid() )
+       {
+           apvts.replaceState(tree);
+           updateFilters();
+       }
 }
 
 
@@ -223,7 +274,7 @@ void Audio::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
         if (bufferToFill.buffer->getNumChannels() > 0)
         {
             auto* channelData = bufferToFill.buffer->getReadPointer (0, bufferToFill.startSample);
- 
+            
             for (auto i = 0; i < bufferToFill.numSamples; ++i)
                 pushNextSampleIntoFifo (channelData[i]);
         }
@@ -246,12 +297,149 @@ void Audio::pushNextSampleIntoFifo (float sample) noexcept
         fifo[fifoIndex++] = sample;
     }
 //
+
+
+
+
+
 void Audio::prepareToPlay(int samplesPerBlock,double sampleRate)
 {
-    
+
 }
 
 void Audio::releaseResources()
 {
     
 }
+
+
+ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts)
+{
+    ChainSettings settings;
+    
+    settings.lowCutFreq = apvts.getRawParameterValue("LowCut Freq")->load();
+    settings.highCutFreq = apvts.getRawParameterValue("HighCut Freq")->load();
+    settings.peakFreq = apvts.getRawParameterValue("Peak Freq")->load();
+    settings.peakGainInDecibels = apvts.getRawParameterValue("Peak Gain")->load();
+    settings.peakQuality = apvts.getRawParameterValue("Peak Quality")->load();
+    settings.lowCutSlope = static_cast<Slope>(apvts.getRawParameterValue("LowCut Slope")->load());
+    settings.highCutSlope = static_cast<Slope>(apvts.getRawParameterValue("HighCut Slope")->load());
+    
+    settings.lowCutBypassed = apvts.getRawParameterValue("LowCut Bypassed")->load() > 0.5f;
+    settings.peakBypassed = apvts.getRawParameterValue("Peak Bypassed")->load() > 0.5f;
+    settings.highCutBypassed = apvts.getRawParameterValue("HighCut Bypassed")->load() > 0.5f;
+    
+    return settings;
+}
+
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+    return juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate,
+                                                               chainSettings.peakFreq,
+                                                               chainSettings.peakQuality,
+                                                               juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+}
+
+void TemperatureSliderAudioProcessor::updatePeakFilter(const ChainSettings &chainSettings)
+{
+    auto peakCoefficients = makePeakFilter(chainSettings, getSampleRate());
+    
+    leftChain.setBypassed<ChainPositions::Peak>(chainSettings.peakBypassed);
+    rightChain.setBypassed<ChainPositions::Peak>(chainSettings.peakBypassed);
+    
+    updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
+    updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
+}
+
+void updateCoefficients(Coefficients &old, const Coefficients &replacements)
+{
+    *old = *replacements;
+}
+
+void TemperatureSliderAudioProcessor::updateLowCutFilters(const ChainSettings &chainSettings)
+{
+    auto cutCoefficients = makeLowCutFilter(chainSettings, getSampleRate());
+    auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
+    auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
+    
+    leftChain.setBypassed<ChainPositions::LowCut>(chainSettings.lowCutBypassed);
+    rightChain.setBypassed<ChainPositions::LowCut>(chainSettings.lowCutBypassed);
+    
+    updateCutFilter(rightLowCut, cutCoefficients, chainSettings.lowCutSlope);
+    updateCutFilter(leftLowCut, cutCoefficients, chainSettings.lowCutSlope);
+}
+
+void TemperatureSliderAudioProcessor::updateHighCutFilters(const ChainSettings &chainSettings)
+{
+    auto highCutCoefficients = makeHighCutFilter(chainSettings, getSampleRate());
+    
+    auto& leftHighCut = leftChain.get<ChainPositions::HighCut>();
+    auto& rightHighCut = rightChain.get<ChainPositions::HighCut>();
+    
+    leftChain.setBypassed<ChainPositions::HighCut>(chainSettings.highCutBypassed);
+    rightChain.setBypassed<ChainPositions::HighCut>(chainSettings.highCutBypassed);
+    
+    updateCutFilter(leftHighCut, highCutCoefficients, chainSettings.highCutSlope);
+    updateCutFilter(rightHighCut, highCutCoefficients, chainSettings.highCutSlope);
+}
+
+void TemperatureSliderAudioProcessor::updateFilters()
+{
+    auto chainSettings = getChainSettings(apvts);
+    
+    updateLowCutFilters(chainSettings);
+    updatePeakFilter(chainSettings);
+    updateHighCutFilters(chainSettings);
+}
+
+juce::AudioProcessorValueTreeState::ParameterLayout TemperatureSliderAudioProcessor::createParameterLayout()
+{
+    juce::AudioProcessorValueTreeState::ParameterLayout layout;
+    
+    layout.add(std::make_unique<juce::AudioParameterFloat>("LowCut Freq",
+                                                           "LowCut Freq",
+                                                           juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
+                                                           20.f));
+    
+    layout.add(std::make_unique<juce::AudioParameterFloat>("HighCut Freq",
+                                                           "HighCut Freq",
+                                                           juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
+                                                           20000.f));
+    
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Freq",
+                                                           "Peak Freq",
+                                                           juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
+                                                           750.f));
+    
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Gain",
+                                                           "Peak Gain",
+                                                           juce::NormalisableRange<float>(-24.f, 24.f, 0.5f, 1.f),
+                                                           0.0f));
+    
+    layout.add(std::make_unique<juce::AudioParameterFloat>("Peak Quality",
+                                                           "Peak Quality",
+                                                           juce::NormalisableRange<float>(0.1f, 10.f, 0.05f, 1.f),
+                                                           1.f));
+    
+    juce::StringArray stringArray;
+    for( int i = 0; i < 4; ++i )
+    {
+        juce::String str;
+        str << (12 + i*12);
+        str << " db/Oct";
+        stringArray.add(str);
+    }
+    
+    layout.add(std::make_unique<juce::AudioParameterChoice>("LowCut Slope", "LowCut Slope", stringArray, 0));
+    layout.add(std::make_unique<juce::AudioParameterChoice>("HighCut Slope", "HighCut Slope", stringArray, 0));
+    
+    layout.add(std::make_unique<juce::AudioParameterBool>("LowCut Bypassed", "LowCut Bypassed", false));
+    layout.add(std::make_unique<juce::AudioParameterBool>("Peak Bypassed", "Peak Bypassed", false));
+    layout.add(std::make_unique<juce::AudioParameterBool>("HighCut Bypassed", "HighCut Bypassed", false));
+    layout.add(std::make_unique<juce::AudioParameterBool>("Analyzer Enabled", "Analyzer Enabled", true));
+    
+    return layout;
+}
+
+
+
